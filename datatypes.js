6cd2c469557c6372a839fa2f408c0530de5895b0
Waleed Hassan <160391842+waleedhassan03@users.noreply.github.com>
2024-03-05 03:07:28 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+// some basic data types in typescript
+// some commonly used data types in TypeScript such as number, string, boolean
+// String
+let myName = "waleed hassan";
+console.log(myName); //string Used to represent textual data, enclosed within single or double quotes
+// Number
+let age = 27;
+console.log(age); //Number Used to represent numeric values
+// Boolean
+let isMarried = false;
+console.log(isMarried); //Boolean Used to represent logical values, either true or false
+// collectively written
+let msg = "hello dear";
+let yourAge = 25;
+let isStudnet = true;
+console.log(msg, yourAge, isStudnet);
---
+++ datatypes.ts
@@ -0,0 +1,22 @@
+// some basic data types in typescript
+// some commonly used data types in TypeScript such as number, string, boolean
+
+// String
+let myName: string = "waleed hassan";
+console.log(myName);     //string Used to represent textual data, enclosed within single or double quotes
+
+
+
+// Number
+let age: number = 27;
+console.log(age);         //Number Used to represent numeric values
+
+// Boolean
+let isMarried: Boolean = false;
+console.log(isMarried);       //Boolean Used to represent logical values, either true or false
+
+// collectively written
+let msg = "hello dear";
+let yourAge = 25;
+let isStudnet = true;
+console.log(msg, yourAge, isStudnet);
